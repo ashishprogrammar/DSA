0396a892ef748a664b73697e7c21d44ad017c5b2
ashishprogrammar <159572308+ashishprogrammar@users.noreply.github.com>
2025-06-05 23:10:23 +0530

    Update leetcode_94(Binary_tree).cpp

---
+++ DSA/Day-26/leetcode_94(Binary_tree).cpp
@@ -0,0 +1,40 @@
+// Recursive Approach
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& result) {
+        if (root == nullptr) return;
+        inorder(root->left, result);
+        result.push_back(root->val);
+        inorder(root->right, result);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        inorder(root, result);
+        return result;
+    }
+};
+
+// Iterative Approach using Stack
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+
+        while (curr != nullptr || !st.empty()) {
+            while (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            curr = st.top();
+            st.pop();
+            result.push_back(curr->val);
+            curr = curr->right;
+        }
+
+        return result;
+    }
+};
